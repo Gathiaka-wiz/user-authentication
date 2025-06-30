82dcafd82b5e027cfe4d059724da575a04a89f94
John Gathiaka <johngathiakaw@gmail.com>
2025-06-30 11:01:05 +0300

    Update verifyToken.js

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -3,12 +3,12 @@ import jwt, { decode } from 'jsonwebtoken';
 const  verifyToken = (req, res, next) => {
     const token = req.cookies.token;
 
-    if (!token)  return res.status(401).json({ message:false, message:"Unauthorized - no token provided" }) ;
+    if (!token)  return res.status(401).json({ success:false, message:"Unauthorized - no token provided" }) ;
     
     try {
         const decoded = jwt.verify(token,process.env.JWT_SECRET);    
         
-        if (!decoded)  return res.status(401).json({ message:false, message:"Unauthorized - invalid token " }) ;
+        if (!decoded)  return res.status(401).json({ success:false, message:"Unauthorized - invalid token " }) ;
 
         req.userId = decoded.userId;
 
